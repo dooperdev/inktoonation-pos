9d388bf3aa52ca5418f8ed76a79143535446d9af
dooperdev <dooper.dev@gmail.com>
2026-04-23 12:36:26 +0800

    Add pin form

---
+++ POS/Forms/Auth/PinForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS.Forms.Auth
+{
+    public partial class PinForm : Form
+    {
+        public PinForm()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            EnableKeyboardShortcuts();
+        }
+
+        private void EnableKeyboardShortcuts()
+        {
+            this.KeyDown += (s, e) =>
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Escape:
+                        this.Close();
+                        break;
+                }
+            };
+        }
+    }
+}
